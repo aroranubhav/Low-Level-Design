3d6ff4a357b4a1ae0c98cced156fa80d4745c09d
maxi <arora.anubhav93@gmail.com>
2025-08-31 19:51:23 +0530

    Update logic to use defaultdict instead of using get

--- TicTacToe/tictactoe/board.py
+++ TicTacToe/tictactoe/board.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
+
 class Board:
     def __init__(self, size):
         self.reset(size)
 
     def reset(self, size):
         self.board = [['' for _ in range(size)] for _ in range(size)]
-        self.rowCounts = {}
-        self.colCounts = {}
-        self.diagCounts = {}
+        self.rowCounts = defaultdict(lambda: defaultdict(int))
+        self.colCounts = defaultdict(lambda: defaultdict(int))
+        self.diagCounts = defaultdict(lambda: defaultdict(int))
         self.size = size
         self.currCounter = 0
     
@@ -25,15 +27,13 @@ class Board:
         self.currCounter += 1
         
         #row 
-        self.rowCounts[x] = self.rowCounts.get(x, {})
-        self.rowCounts[x][identifier] = self.rowCounts[x].get(identifier, 0) + 1
-
+        self.rowCounts[x][identifier] += 1
+        
         if self.rowCounts[x][identifier] == self.size:
             return True
             
         #column
-        self.colCounts[y] = self.colCounts.get(y, {})
-        self.colCounts[y][identifier] = self.colCounts[y].get(identifier, 0) + 1
+        self.colCounts[y][identifier] += 1
 
         if self.colCounts[y][identifier] == self.size:
             return True
@@ -43,17 +43,15 @@ class Board:
 
         #forward
         if x == y:
-            self.diagCounts['forwards'] = self.diagCounts.get('forwards', {})
-            self.diagCounts['forwards'][identifier] = self.diagCounts['forwards'].get(identifier, 0) + 1
-
+            self.diagCounts['forwards'][identifier] += 1
+        
             if self.diagCounts['forwards'][identifier] == self.size:
                 return True
             
         #backward
         if x + y == self.size - 1:
-            self.diagCounts['backwards'] = self.diagCounts.get('backwards', {})
-            self.diagCounts['backwards'][identifier] = self.diagCounts['backwards'].get(identifier, 0) + 1
-
+            self.diagCounts['backwards'][identifier] += 1
+        
             if self.diagCounts['backwards'][identifier] == self.size:
                 return True
             
